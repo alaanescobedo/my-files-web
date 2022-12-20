0a5191cc81809f674c73b4fc6617184e2d7b2061
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-12-20 15:33:01 -0600

    middleware

--- middleware.ts
+++ middleware.ts
@@ -8,22 +8,20 @@ export async function middleware(request: NextRequest) {
 
   if (request.nextUrl.pathname.startsWith('/user')) {
     const atCookie = request.cookies.get(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME || "");
-    console.log('user', { atCookie })
-    // try {
-    //   await authService.authenticate(`${atCookie?.name}=${atCookie?.value}`)
-    // } catch (error: any) {
-    //   return NextResponse.redirect(new URL('/auth/login', request.url))
-    // }
+    try {
+      await authService.authenticate(`${atCookie?.name}=${atCookie?.value}`)
+    } catch (error: any) {
+      return NextResponse.redirect(new URL('/auth/login', request.url))
+    }
   }
   if (request.nextUrl.pathname.startsWith('/auth')) {
     const atCookie = request.cookies.get(process.env.JWT_ACCESS_TOKEN_COOKIE_NAME || "");
-    console.log('cookie', { atCookie })
-    // try {
-    //   await authService.authenticate(`${atCookie?.name}=${atCookie?.value}`)
-    //   return NextResponse.redirect(new URL('/', request.url))
-    // } catch (error: any) {
-    //   return NextResponse.next()
-    // }
+    try {
+      await authService.authenticate(`${atCookie?.name}=${atCookie?.value}`)
+      return NextResponse.redirect(new URL('/', request.url))
+    } catch (error: any) {
+      return NextResponse.next()
+    }
   }
 
 }
