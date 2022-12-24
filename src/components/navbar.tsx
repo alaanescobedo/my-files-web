6cce2534522cfde0168d70961da67e1b50a9ce19
Alan Escobedo <alanescobedo.dev@gmail.com>
2022-12-23 19:08:02 -0600

    refactor: remove unused navburger

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -52,21 +52,6 @@ export default function Navbar() {
         borderStyle={'solid'}
         borderColor={useColorModeValue('gray.200', 'gray.900')}
         align={'center'}>
-        {showFull && (
-          <Flex
-            flex={{ base: 1, md: 'auto' }}
-            ml={{ base: -2 }}
-            display={{ base: 'flex', md: 'none' }}>
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-              }
-              variant={'ghost'}
-              aria-label={'Toggle Navigation'}
-            />
-          </Flex>
-        )}
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
           <Link
             as={NextLink}
@@ -108,7 +93,7 @@ export default function Navbar() {
             </Button>
             <Button
               as={NextLink}
-              display={{ base: 'none', md: 'inline-flex' }}
+              display={ 'inline-flex' }
               fontSize={'sm'}
               fontWeight={600}
               color={'white'}
